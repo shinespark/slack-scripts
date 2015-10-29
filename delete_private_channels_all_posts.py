d41ac55bd80bc0738821aa1944ba4614611375c1
shinespark <shinespark64@gmail.com>
2015-10-29 20:21:18 +0900

    split utils.py

--- delete_private_channels_all_posts.py
+++ delete_private_channels_all_posts.py
@@ -1,7 +1,6 @@
 # coding: utf-8
-import json
-import urllib.request
 import yaml
+import utils
 
 
 def main():
@@ -12,54 +11,29 @@ def main():
     target_group_name = conf['group_name']
 
     # fetch users.list
-    users_list = fetch(end_point + 'users.list' + token)
+    users_list = utils.fetch(end_point + 'users.list' + token)
     your_id = [member['id'] for member in users_list['members'] if member.get('name') == your_name][0]
     print(your_id)
 
     # fetch groups.list
-    groups_list = fetch(end_point + 'groups.list' + token)
+    groups_list = utils.fetch(end_point + 'groups.list' + token)
     target_group_id = [group['id'] for group in groups_list['groups'] if group.get('name') == target_group_name][0]
     print(target_group_id)
 
     # fetch groups.history
-    groups_history = fetch_all_history(end_point + 'groups.history' + token + '&channel=' + target_group_id + '&count=1000')
+    groups_history = utils.fetch_all_history(end_point + 'groups.history' + token + '&channel=' + target_group_id + '&count=1000')
     your_posts_list = [message for message in groups_history if message.get('user') == your_id and message.get('subtype', '') == '']
 
     for message in your_posts_list:
         print(message['text'], message['ts'])
 
     # chat.delete
+    print(' {0} 件削除します'.format(len(your_posts_list)))
     for message in your_posts_list:
-        delete_status = fetch(end_point + 'chat.delete' + token + '&ts=' + message['ts'] + '&channel=' + target_group_id)
+        delete_status = utils.fetch(end_point + 'chat.delete' + token + '&ts=' + message['ts'] + '&channel=' + target_group_id)
         print(delete_status)
 
     print('complete!!')
-
-
-def fetch(url):
-    print(url)
-    res = urllib.request.urlopen(url)
-    res_body = json.loads(res.read().decode('utf-8'))
-    return res_body
-
-
-def fetch_all_history(url, base_url='', all_list=[]):
-    print(url)
-
-    # init
-    if base_url == '':
-        base_url = url
-
-    res = urllib.request.urlopen(url)
-    res_body = json.loads(res.read().decode('utf-8'))
-    all_list += res_body['messages']
-
-    if res_body['has_more']:
-        latest = res_body['messages'][-1]['ts']
-        url = base_url + '&latest=' + latest
-        fetch_all_history(url, base_url, all_list)
-
-    return all_list
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import json
+import urllib.request
+
+
+def fetch(url):
+    print(url)
+    res = urllib.request.urlopen(url)
+    res_body = json.loads(res.read().decode('utf-8'))
+    return res_body
+
+
+def fetch_all_history(url, base_url='', all_list=[]):
+    print(url)
+
+    # init
+    if base_url == '':
+        base_url = url
+
+    res = urllib.request.urlopen(url)
+    res_body = json.loads(res.read().decode('utf-8'))
+    all_list += res_body['messages']
+
+    if res_body['has_more']:
+        latest = res_body['messages'][-1]['ts']
+        url = base_url + '&latest=' + latest
+        fetch_all_history(url, base_url, all_list)
+
+    return all_list
