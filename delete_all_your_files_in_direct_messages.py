b0fd3803bd27e5cff129a7a2871d5f808cf993f2
shinespark <shinespark64@gmail.com>
2015-11-02 17:51:38 +0900

    add delete_ims_*

---
+++ delete_all_your_files_in_direct_messages.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import utils
+
+
+def main():
+    argvs = sys.argv
+    if len(argvs) != 4:
+        print('usage:\n    delete_all_your_files_in_direct_messages.py <Slack Web API token> <Your Slack name> <target_user_name>\n')
+        exit()
+
+    end_point = 'https://slack.com/api/'
+    token, your_name, target_user_name = argvs[1:]
+    token = '?token=' + token
+
+    # fetch users.list
+    users_list = utils.fetch(end_point + 'users.list' + token)
+    your_id = [member['id'] for member in users_list['members'] if member.get('name') == your_name][0]
+    target_user_id = [member['id'] for member in users_list['members'] if member.get('name') == target_user_name][0]
+    print('your_id: ' + your_id)
+    print('target_user_id: ' + target_user_id)
+
+    # fetch im.list
+    im_list = utils.fetch(end_point + 'im.list' + token)
+    target_im_id = [im['id'] for im in im_list['ims'] if im.get('user') == target_user_id][0]
+    print('target_im_id: ' + target_im_id)
+
+    # fetch files.list
+    your_files_list = utils.fetch_all_files(end_point + 'files.list' + token + '&user=' + your_id)
+    target_ims_your_files_list = [f for f in your_files_list if target_im_id in f.get('ims')]
+
+    # show your files
+    for f in target_ims_your_files_list:
+        print(f['id'], f['url_private'])
+
+    # files.delete
+    print('------------------------------')
+    print('{0} 件削除します。よろしいですか？'.format(len(target_ims_your_files_list)))
+    while True:
+        ans = input('[y/n] > ')
+        if ans in 'yYnN' and ans != '':
+            break
+
+    if ans == 'y' or ans == 'Y':
+        for f in target_ims_your_files_list:
+            print(f['id'], f['url_private'])
+            delete_status = utils.fetch(end_point + 'files.delete' + token + '&file=' + f['id'])
+            print(delete_status)
+
+        print('complete!!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ delete_all_your_posts_in_direct_messages.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import utils
+
+
+def main():
+    argvs = sys.argv
+    if len(argvs) != 4:
+        print('usage:\n    delete_all_your_posts_in_direct_messages.py <Slack Web API token> <Your Slack name> <target_user_name>\n')
+        exit()
+
+    end_point = 'https://slack.com/api/'
+    token, your_name, target_user_name = argvs[1:]
+    token = '?token=' + token
+
+    # fetch users.list
+    users_list = utils.fetch(end_point + 'users.list' + token)
+    your_id = [member['id'] for member in users_list['members'] if member.get('name') == your_name][0]
+    target_user_id = [member['id'] for member in users_list['members'] if member.get('name') == target_user_name][0]
+    print('your_id: ' + your_id)
+    print('target_user_id: ' + target_user_id)
+
+    # fetch im.list
+    im_list = utils.fetch(end_point + 'im.list' + token)
+    target_im_id = [im['id'] for im in im_list['ims'] if im.get('user') == target_user_id][0]
+    print('target_im_id: ' + target_im_id)
+
+    # fetch im.history
+    im_history = utils.fetch_all_history(end_point + 'im.history' + token + '&channel=' + target_im_id + '&count=1000')
+    your_posts_list = [message for message in im_history if message.get('user') == your_id and message.get('subtype', '') == '']
+
+    # show your posts
+    for message in your_posts_list:
+        print(message['text'].replace('\n', ''), message['ts'])
+
+    # chat.delete
+    print('------------------------------')
+    print('{0} 件削除します。よろしいですか？'.format(len(your_posts_list)))
+    while True:
+        ans = input('[y/n] > ')
+        if ans in 'yYnN' and ans != '':
+            break
+
+    if ans == 'y' or ans == 'Y':
+        for message in your_posts_list:
+            print(message['text'].replace('\n', ''), message['ts'])
+            delete_status = utils.fetch(end_point + 'chat.delete' + token + '&ts=' + message['ts'] + '&channel=' + target_im_id)
+            print(delete_status)
+
+        print('complete!!')
+
+
+if __name__ == "__main__":
+    main()
