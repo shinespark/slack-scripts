07270329a4a98aa5803c9236902e4b8fb275b884
shinespark <shinespark64@gmail.com>
2017-11-22 20:21:12 +0900

    minor fix

--- delete_all_files.py
+++ delete_all_files.py
@@ -18,16 +18,16 @@ def main():
     files_list = utils.fetch_all_files(end_point + 'files.list' + token)
 
     # show your files
-    for f in target_files_list:
+    for f in files_list:
         print(f['id'], f['url_private'])
 
     # files.delete
     print('------------------------------')
-    print('{0} 件削除します。よろしいですか？'.format(len(target_your_files_list)))
+    print('{0} 件削除します。よろしいですか？'.format(len(files_list)))
     ans = utils.prompt()
 
     if ans == 'y' or ans == 'Y':
-        for f in target_files_list:
+        for f in files_list:
             print(f['id'], f['url_private'])
             delete_status = utils.fetch(end_point + 'files.delete' + token + '&file=' + f['id'])
             print(delete_status)
